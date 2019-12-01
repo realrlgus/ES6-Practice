c5ebfa6b60e4bfa08f4c1ab9f78bd26a3ad88a26
realrlgus <realrlgus@naver.com>
2019-12-01 15:48:16 +0900

    practice class

---
+++ class/app.js
@@ -0,0 +1,28 @@
+class Counter {
+  constructor({ initialNumber = 0, counterId, plusId, minusId }) {
+    this.count = initialNumber;
+    this.counter = document.getElementById(counterId);
+    this.counter.innerText = initialNumber;
+    this.plusBtn = document.getElementById(plusId);
+    this.minusBtn = document.getElementById(minusId);
+    this.addEventListeners();
+  }
+
+  addEventListeners = () => {
+    this.plusBtn.addEventListener("click", this.increase);
+    this.minusBtn.addEventListener("click", this.decrease);
+  };
+  increase = () => {
+    this.count = this.count + 1;
+    this.repaintCount();
+  };
+  decrease = () => {
+    this.count = this.count - 1;
+    this.repaintCount();
+  };
+  repaintCount = () => {
+    this.counter.innerText = this.count;
+  };
+}
+
+new Counter({ counterId: "count", plusId: "add", minusId: "minus" });
